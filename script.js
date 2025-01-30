d2bbc89b7120445d0502e65cf32e72cbdcdde5e0
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-01-30 11:28:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const accordians = document.querySelectorAll(".accordian");
+
+accordians.forEach((acc) => {
+  const icon = acc.querySelector(".down-icon");
+  const answer = acc.querySelector(".answer");
+
+  acc.addEventListener("click", () => {
+    
+    if (icon.classList.contains("active")) {
+      icon.classList.remove("active");
+      answer.style.maxHeight = null;
+    } else {
+      icon.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
